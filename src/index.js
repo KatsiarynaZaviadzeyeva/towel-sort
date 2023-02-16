c5d77d961c3f0577b14b250c280e4bc87763a212
KatsiarynaZaviadzeyeva <katsiarynaappleby@gmail.com>
2023-02-16 15:22:01 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
+    let array = matrix.reduce((acc, elem, ind) => {
+        elem.sort((a, b) => !(ind & 1) ? a - b : b - a).map(e => acc.push(e));
+        return acc;
+    }, []);
+    return array;
 }
